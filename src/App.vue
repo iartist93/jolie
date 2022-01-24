d26e96f1faf82e17614eb9e5a8cb6d4cd0901a2c
ahmad <a.ayman@uniparticle.com>
2022-01-24 13:21:48 +0200

    fix: conflict between `sass-loader` and `vue` versions
    see this error:
    https://stackoverflow.com/questions/66082397/typeerror-this-getoptions-is-not-a-function

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <sample-component />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import SampleComponent from './components/SampleComponent.vue';
 
 export default Vue.extend({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    SampleComponent,
+  },
 });
 </script>
 
