8c0b799b100aec0dde66aebfed8ac3f7da363279
ahmad <a.ayman@uniparticle.com>
2022-02-01 12:51:12 +0200

    fix: remove lib folder from tracked files

--- lib/install.d.ts
+++
@@ -1,5 +0,0 @@
-import Box from './components/Box.vue';
-import IconButton from './components/IconButton.vue';
-import Toggle from './components/Toggle.vue';
-import SampleComponent from './components/SampleComponent.vue';
-export { IconButton, SampleComponent, Box, Toggle };
