01a854c971447924fd3659cad10a14a721713fbc
ahmad <a.ayman@uniparticle.com>
2022-02-16 23:39:27 +0200

    feat: install vuePress plugin

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const vueSrc = '../../src';
+
+module.exports = {
+  dest: 'vuepress',
+  serviceWorker: true,
+  themeConfig: {
+    sidebar: ['/', '/page1', '/page2', ['/page3', 'Custom link page']],
+  },
+  title: 'jolie',
+  base: '/jolie/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, vueSrc),
+      },
+    },
+  },
+  scss: {
+    additionalData: `@import "@/assets/css/colors.scss";
+                     @import "@/assets/css/fonts.scss";
+                     @import "@/assets/css/highlightableTextarea.scss";
+                     @import "@/assets/css/main.scss";
+    `,
+  },
+};
